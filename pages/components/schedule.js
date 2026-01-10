8071b854cfb4706b26b1416544caf22f885cc8d4
Ram Basnet <rambasnet@gmail.com>
2026-01-09 23:12:42 -0700

    update schedule

--- pages/components/schedule.js
+++ pages/components/schedule.js
@@ -7,47 +7,47 @@ export default class Schedule extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            semester: "Fall 2025 Schedule",
+            semester: "Spring 2026 Schedule",
             time: ["9:00 am","2:00 pm"], // time range for schedule
             day: ["Mon", "Fri"], // day range for schedule
             
             // days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
             items: [
                 {
+                    name: "CS 1",
+                    loc: "CH 276",
+                    days: ["Mon","Tue","Wed", "Thu"],
+                    time: ["9:00 am", "9:50 am"],
+                },
+                {
+                    name: "Python Machine Learning",
+                    loc: "CH 210",
+                    days: ["Mon", "Wed", "Fri"],
+                    time: ["10:00 am", "10:50 am"],
+                },
+                {
+                    name: "OOP & Design Patterns",
+                    loc: "CH 310",
+                    days: ["Tue", "Thu"],
+                    time: ["11:00 am", "12:15 pm"],
+                },
+                {
                     name: "Office Hour",
                     loc: "CH 329",
                     days: ["Mon","Wed", "Fri"],
-                    time: ["10:00 am", "10:50 am"],
+                    time: ["11:00 am", "11:50 am"],
+                },
+                {
+                    name: "Software Security",
+                    loc: "CH 205",
+                    days: ["Mon", "Wed", "Fri"],
+                    time: ["1:00 pm", "1:50 pm"],
                 },
                 {
                     name: "Office Hour",
                     loc: "CH 329",
                     days: ["Tue","Thu"],
                     time: ["1:00 pm", "1:50 pm"],
-                },
-                {
-                    name: "CS0 - Beg. Python",
-                    loc: "CH 276",
-                    days: ["Mon", "Wed", "Fri"],
-                    time: ["9:00 am", "9:50 am"],
-                },
-                {
-                    name: "CS0 - Lab",
-                    loc: "CH 276",
-                    days: ["Tue", "Thu"],
-                    time: ["9:00 am", "9:50 am"],
-                },
-                {
-                    name: "OOP & Design Patterns",
-                    loc: "CH 215",
-                    days: ["Tue", "Thu"],
-                    time: ["11:00 am", "12:15 pm"],
-                },
-                {
-                    name: "Net/App Security",
-                    loc: "CH 210",
-                    days: ["Mon", "Wed", "Fri"],
-                    time: ["11:00 am", "11:50 am"],
                 },
             ]
         };
